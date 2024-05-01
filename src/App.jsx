e188a4805d229a7de68ff5875a34818b12440637
Bijay <sthabeejay2060@gmail.com>
2024-05-01 13:17:46 +0545

    DigitalFoodsFrontend

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+
+import './App.css'
+import {  BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './globals/components/navbar/Navbar'
+import Footer from './globals/components/footer/Footer'
+import { Provider } from 'react-redux'
+import store from './store/store'
+import Home from './pages/home/Home'
+import Login from './pages/auth/login/Login'
+import Cart from './pages/cart/Cart'
+import Register from './pages/auth/register/Register'
+import ProductDetails from './pages/productDetails/ProductDetails'
+import CheckOut from './pages/checkout/CheckOut'
+import KhaltiSuccess from './pages/success/KhaltiSuccess'
+import UserProfile from './pages/profile/UserProfile'
+import MyOrders from './pages/myOrders/MyOrders'
+import OrderDetails from './pages/orderDetails/OrderDetails'
+import MyOrderQrs from './pages/MyOrderQrs/MyOrderQrs'
+import ForgotPassword from './pages/auth/forgotPassword/ForgotPassword'
+import VerifyOtp from './pages/auth/verifyOtp/VerifyOtp'
+import ResetPassword from './pages/auth/resetPassword/ResetPassword'
+// import AdminDashboard from './pages/admin/dashboard/AdminDashboard'
+// import ProtectedRoute, { ProctectedRouteForVendor } from './pages/ProtectRoute'
+import {io} from 'socket.io-client'
+export const socket = io("http://localhost:3000",{
+  auth : {
+    token : localStorage.getItem('token')
+  }
+})
+
+
+function App() {
+
+
+  return (
+    <>
+   <Provider store={store}>
+   {/* <Navbar />
+   <RouterProvider router={router} />
+   <Footer /> */}
+   <BrowserRouter>
+   <Navbar />
+   <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    <Route path="/cart" element={<Cart />} />
+    <Route path="/productdetails/:id" element={<ProductDetails />} />
+    <Route path="/checkout" element={<CheckOut />} />
+   <Route path="/success" element={<KhaltiSuccess />} />
+   <Route path="/profile" element={<UserProfile />} />
+   <Route path="/myorders" element={<MyOrders />} />
+   <Route path="/myorderqrs" element={<MyOrderQrs />} />
+   <Route path="/myorders/:id" element={<OrderDetails />} />
+   <Route path="/forgotpassword" element={<ForgotPassword />} />
+   <Route path="/verifyotp" element={<VerifyOtp />} />
+   <Route path="/resetpassword" element={<ResetPassword />} />
+   {/* <Route path="/admin" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+   <Route path="/forgotpassword" element={<ForgotPassword />} />
+   {/* <Route path="/admin" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+   <Route path="/vendor" element={<ProctectedRouteForVendor><VendorDashboard /></ProctectedRouteForVendor>} /> */}
+   </Routes>
+   <Footer/>
+   </BrowserRouter>
+   </Provider>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/store/productSlice.js
@@ -0,0 +1,82 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {API} from "../http";
+
+
+const STATUSES = Object.freeze({
+    SUCCESS : 'success',
+ERROR : 'error',
+    LOADING : 'loading'
+})
+
+
+const productSlice = createSlice({
+    name : "product",
+    initialState :{
+        data : [],
+        status : STATUSES.SUCCESS,
+        selectedProduct : {}
+    },
+    reducers : {
+       setProducts(state,action){
+        state.data = action.payload
+       },
+       setStatus(state,action){
+        state.status = action.payload
+       },
+       setselectedProduct(state,action){
+        state.selectedProduct = action.payload
+       }
+    },
+    extraReducers : (builder) =>{
+        builder
+        .addCase(fetchProducts.pending,(state)=>{
+            state.status = STATUSES.LOADING
+        })
+        .addCase(fetchProducts.fulfilled,(state,action)=>{
+            state.data = action.payload 
+            state.status = STATUSES.SUCCESS
+        })
+        .addCase(fetchProducts.rejected,(state)=>{
+            state.status = STATUSES.ERROR
+        })
+    }
+
+})
+
+export const {setProducts,setStatus,setselectedProduct} = productSlice.actions 
+
+export default productSlice.reducer 
+
+export const fetchProducts = createAsyncThunk('products/fetch',async()=>{
+    const response = await API.get("/products")
+    const data = response.data.data 
+    return data
+})
+
+// export function fetchProducts(){
+//     return async function fetchProductThunk(dispatch){
+//         dispatch(setStatus(STATUSES.LOADING))
+//         try {
+//             const response = await axios.get("http://localhost:3000/api/products")
+//             dispatch(setProducts(response.data.data))
+//            dispatch(setStatus(STATUSES.SUCCESS))
+//         } catch (error) {
+//             console.log(error)
+//             dispatch(setStatus(STATUSES.ERROR))
+//         }
+//     }
+// }
+
+export function fetchProductDetails(productId){
+    return async function fetchProductDetailsThunk(dispatch){
+        dispatch(setStatus(STATUSES.LOADING))
+        try {
+            const response = await API.get(`products/${productId}`)
+            dispatch(setselectedProduct(response.data.data))
+            dispatch(setStatus(STATUSES.SUCCESS))
+        } catch (error) {
+            console.log(error)
+            dispatch(setStatus(STATUSES.ERROR))
+        }
+    }
+}
